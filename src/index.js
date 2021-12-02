f52e31b243feb9647d6882cc2e515067897cafe9
Chris Masters <chrismasters@gmail.com>
2021-12-02 14:17:47 +0100

    Added locations and genres modals

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ if (streamsModalEl) {
   var streamsModal = new boosted.Modal(streamsModalEl);
   streamsModal.show();
 }
+
+var genresModalEl = document.getElementById('genresModal');
+
+if (genresModalEl) {
+  var genresModal = new boosted.Modal(genresModalEl);
+  genresModal.show();
+}
+
+var locationsModalEl = document.getElementById('locationsModal');
+
+if (locationsModalEl) {
+  var locationsModal = new boosted.Modal(locationsModalEl);
+  locationsModal.show();
+}
