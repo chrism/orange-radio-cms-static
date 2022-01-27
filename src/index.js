8b8f7a870f924863042997e7feeef7acb0287a40
Chris Masters <chrismasters@gmail.com>
2022-01-27 12:06:51 +0100

    Added delete confirmation modal. Closes #6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as boosted from 'boosted';
 
-const modals = ['streamsModal', 'genresModal', 'locationsModal', 'rssModal']
+const modals = ['streamsModal', 'genresModal', 'locationsModal', 'rssModal', 'deleteConfirmationModal']
 
 modals.forEach(modal => {
   const modalEl = document.getElementById(modal)
