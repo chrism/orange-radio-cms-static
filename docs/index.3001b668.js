605c4e9492593dc731b39228881616b6ac0a8dad
Chris Masters <chrismasters@gmail.com>
2023-05-03 13:21:01 +0200

    Working with latest parcel

---
+++ docs/index.3001b668.js
@@ -0,0 +1,29 @@
+import {Modal as $h0jcB$Modal} from "boosted";
+import $h0jcB$sortablejs from "sortablejs";
+
+
+
+const $cf838c15c8b009ba$var$modals = [
+    "streamsModal",
+    "genresModal",
+    "locationsModal",
+    "rssModal",
+    "deleteConfirmationModal",
+    "episodeStreamsModal",
+    "addItemModal",
+    "addCoverModal",
+    "countryModal"
+];
+$cf838c15c8b009ba$var$modals.forEach((modal)=>{
+    const modalEl = document.getElementById(modal);
+    if (!modalEl) return;
+    const boostedModal = new $h0jcB$Modal(modalEl);
+    boostedModal.show();
+});
+var $cf838c15c8b009ba$var$sortableEl = document.getElementById("sortableItems");
+if (!!$cf838c15c8b009ba$var$sortableEl) (0, $h0jcB$sortablejs).create($cf838c15c8b009ba$var$sortableEl, {
+    animation: 150
+});
+
+
+//# sourceMappingURL=index.3001b668.js.map
